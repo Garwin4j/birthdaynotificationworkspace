dc0cf542bca14fe657b04d0cac19e35795583e43
Garwin Pryce <garwin.pryce@gmailcom>
2013-03-22 00:44:56 -0500

    Code finish for 10 Coloum Format
    Finish most of the coding but not working

--- TextService/AlertService/AlertService.svc.cs
+++ TextService/AlertService/AlertService.svc.cs
@@ -6,6 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 using System.Diagnostics;
+using System.IO;
 
 namespace AlertService
 {
@@ -13,12 +14,18 @@ namespace AlertService
     public class AlertService : IAlertService
     {
         static TraceSource trace;
+        static StreamWriter EmailLog;
+        static StreamWriter TextLog;
         static AlertService()
         {
-            trace = new TraceSource("ServiceOutput");            
+            trace = new TraceSource("ServiceOutput");
+            EmailLog = File.AppendText("Email.log");
+            TextLog = File.AppendText("Text.log");
         }
+
         public string sendSMS(string MobileNumber, string Message, string SendingCompany)
         {
+            TextLog.WriteLine(String.Format("Send SMS to {0}{1}Message: {2}", MobileNumber, Environment.NewLine, Message));
             trace.TraceInformation(String.Format("Send SMS to {0}{1}Message: {2}",MobileNumber,Environment.NewLine,Message));
             return "Success";
 
@@ -26,6 +33,10 @@ namespace AlertService
 
         public string sendEmail(string EmailAddress, string From, string Subject, string Body, string Attachment, string SendingCompany, string SendingApplication)
         {
+            EmailLog.WriteLine("----------------------------------------------------------------");
+            EmailLog.WriteLine(String.Format("Send Email to {0}{1}Message: {2}", EmailAddress, Environment.NewLine, Body));
+            EmailLog.WriteLine("----------------------------------------------------------------");
+
             trace.TraceInformation(String.Format("Send Email to {0}{1}Message: {2}", EmailAddress, Environment.NewLine, Body));
             return "Success";
         }
