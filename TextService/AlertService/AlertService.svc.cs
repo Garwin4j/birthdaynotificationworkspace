275da3762954ab92a9072002341a28a5c71097ae
Garwin Pryce <garwin.pryce@gmailcom>
2013-03-22 03:56:33 -0500

    JMMB_Notification - 0.5.0
    * 10 Coloum Format
    * NotificationType via Database
    * Alert service write log to c:\

--- TextService/AlertService/AlertService.svc.cs
+++ TextService/AlertService/AlertService.svc.cs
@@ -14,31 +14,38 @@ namespace AlertService
     public class AlertService : IAlertService
     {
         static TraceSource trace;
-        static StreamWriter EmailLog;
-        static StreamWriter TextLog;
+      
         static AlertService()
         {
             trace = new TraceSource("ServiceOutput");
-            EmailLog = File.AppendText("Email.log");
-            TextLog = File.AppendText("Text.log");
+           
+           
         }
 
         public string sendSMS(string MobileNumber, string Message, string SendingCompany)
-        {
-            TextLog.WriteLine(String.Format("Send SMS to {0}{1}Message: {2}", MobileNumber, Environment.NewLine, Message));
-            trace.TraceInformation(String.Format("Send SMS to {0}{1}Message: {2}",MobileNumber,Environment.NewLine,Message));
-            return "Success";
-
+        {           
+            using( StreamWriter TextLog = File.AppendText(@"c:\Text.log"))
+	        {
+		        TextLog.WriteLine(String.Format("Send SMS to {0}{1}Message: {2}", MobileNumber, Environment.NewLine, Message));
+                trace.TraceInformation(String.Format("Send SMS to {0}{1}Message: {2}",MobileNumber,Environment.NewLine,Message));
+                TextLog.Flush();
+                return "Success";
+	        }
         }
 
         public string sendEmail(string EmailAddress, string From, string Subject, string Body, string Attachment, string SendingCompany, string SendingApplication)
         {
-            EmailLog.WriteLine("----------------------------------------------------------------");
-            EmailLog.WriteLine(String.Format("Send Email to {0}{1}Message: {2}", EmailAddress, Environment.NewLine, Body));
-            EmailLog.WriteLine("----------------------------------------------------------------");
-
-            trace.TraceInformation(String.Format("Send Email to {0}{1}Message: {2}", EmailAddress, Environment.NewLine, Body));
-            return "Success";
+            
+            
+            using (StreamWriter EmailLog = File.AppendText(@"c:\Email.log"))
+            {            
+                EmailLog.WriteLine("----------------------------------------------------------------");
+                EmailLog.WriteLine(String.Format("Send Email to {0}{1}Message: {2}", EmailAddress, Environment.NewLine, Body));
+                EmailLog.WriteLine("----------------------------------------------------------------");
+                EmailLog.Flush();
+                trace.TraceInformation(String.Format("Send Email to {0}{1}Message: {2}", EmailAddress, Environment.NewLine, Body));
+                return "Success";
+            }
         }
     }
 }
