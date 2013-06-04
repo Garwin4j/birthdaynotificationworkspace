83e0b4aff54df670dbbe23ececb9034de585aa78
Garwin Pryce - Win8 <garwin.pryce@gmail.com>
2013-06-04 01:30:10 -0500

    added primary key to MissingInformation

--- MessageNotificationConsole/deploy/JMMB_CustomerNotification/Tables/MissingInformation.sql
+++ MessageNotificationConsole/deploy/JMMB_CustomerNotification/Tables/MissingInformation.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[MissingInformation] (
-    [NotificationType]    NVARCHAR (50) NULL,
+    [MissingInformationId] INT NOT NULL IDENTITY,
+	[NotificationType]    NVARCHAR (50) NULL,
     [CustomerID]          NVARCHAR (50) NULL,
     [MissingTelephone]    NVARCHAR (3)  NULL,
     [MissingEmailAddress] NVARCHAR (3)  NULL,
-    [Date]                DATETIME      NULL
+    [Date]                DATETIME      NULL, 
+    CONSTRAINT [PK_MissingInformation] PRIMARY KEY ([MissingInformationId])
 );
 
