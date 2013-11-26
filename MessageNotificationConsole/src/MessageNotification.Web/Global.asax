1b6cfaa450e7e66289d284ee9d65a001619aed83
Garwin4j <garwin.pryce@gmail.com>
2013-11-26 12:38:00 -0500

    Removed web projects to add again

--- MessageNotificationConsole/src/MessageNotification.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MessageNotification.Web.MvcApplication" Language="C#" %>
--- MessageNotificationConsole/src/MessageNotification.Web/app/services/DataService.js
+++
@@ -1,9 +0,0 @@
-﻿angular.module("myApp").factory("dataService", function ($resource)
-{
-    var dataService = {};
-
-    dataService.notifications = $resource("http://localhost:1630/api/Notifications/:notificationId", { notificationId: "@notificationId" });
-    dataService.currentNotification = {};
-
-    return dataService;
-});
